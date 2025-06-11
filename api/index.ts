1a9f11bd0a16ece815647fccac8d6be237053fc1
Thisbe Scholfield-Johnson <ts1299@georgetown.edu>
2025-06-11 11:49:18 -0600

    chore: add error logging for debugging

--- api/index.ts
+++ api/index.ts
@@ -14,6 +14,10 @@ const spotifyApi = new SpotifyWebApi({
   redirectUri: process.env.YOUR_REDIRECT_URI,
   refreshToken: process.env.YOUR_REFRESH_TOKEN,
 })
+console.log('CLIENT_ID:', process.env.YOUR_CLIENT_ID)
+console.log('REDIRECT_URI:', process.env.YOUR_REDIRECT_URI)
+console.log('REFRESH_TOKEN:', process.env.YOUR_REFRESH_TOKEN)
+
 
 app.get('/', async (_, res) => {
   console.log('✅ Hit the root /api route')
@@ -26,9 +30,11 @@ app.get('/', async (_, res) => {
     console.log('🎵 Now playing response:', current.body)
 
     res.status(200).json(current.body)
-  } catch (err) {
-    console.error('❌ Spotify error:', err)
-    res.status(500).json({ error: 'Spotify API failed' })
+  } catch (error: any) {
+    console.error('Error while fetching current playing track:', error.message)
+    console.error(error.stack)
+    res.status(500).send('Internal Server Error')
+    return
   }
 })
 
